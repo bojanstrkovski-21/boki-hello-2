e5e59d5b963ca266a2c2dcc514508a7a8c4d4d31
bojanstrkovski-21 <115637403+bojanstrkovski-21@users.noreply.github.com>
2023-04-23 17:35:04 +0200

    upd

--- boki-hello-2.sh
+++ boki-hello-2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yad --title "BOKI-HELLO-2" --form --center --borders=15 --columns=3 --width=540 --height=190 --window-icon=icons/logo.png --text="W E L C O M E  T O  B O K I - H E L L O - 2 !!!" --text-align=center --image=icons/logo.png \
+yad --title "BOKI-HELLO-2" --form --center --borders=15 --columns=3 --width=540 --height=190 --window-icon=icons/logo.png --text="W E L C O M E  T O  B O K I - H E L L O - 2 !!!" --text-align=center --image=icons/logo.png --noclose -e \
 --field="<b>Update System Now</b>":fbtn "konsole --noclose -e sh './scripts/sysupd'" \
 --field="<b>Install Arch/AUR Packages</b>":fbtn "konsole --noclose -e sh './scripts/arch'" \
 --field="<b>Install Drivers</b>":fbtn "konsole --noclose -e sh './scripts/drivers'" \
